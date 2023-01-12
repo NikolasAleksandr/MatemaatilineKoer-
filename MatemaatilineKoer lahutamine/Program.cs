e683c4895e7bda0e957b2d9cc6581de1ba6085a5
сашка <@->
2023-01-12 14:58:38 +0300

    Добавьте файлы проекта.

---
+++ MatemaatilineKoer lahutamine/Program.cs
@@ -0,0 +1,116 @@
+﻿namespace MatemaatilineKoer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Matemaatiline Koer");
+
+            Console.WriteLine("Palun sisesta esimene arv");
+            float firstNr = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Palun sisesta tehe: ");
+            string operation = Console.ReadLine();
+
+            Console.WriteLine("Palun sisesta teine arv");
+            float secondNr = int.Parse(Console.ReadLine());
+
+
+
+            try
+            {
+                switch (operation)
+                {
+                    case "+":
+                        float resultAdd = firstNr + secondNr;
+                        Console.WriteLine("Kahe numbri liitmisel on vastus: " + resultAdd);
+                        break;
+
+                    default:
+                        break;
+
+                }
+            }
+            catch (FormatException)//numbrilise v''rtuse kontroll
+            {
+                Console.WriteLine("Vale numbri formaat. Kas sa ei saa siis aru.");
+                //throw;
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("ERROR. Sisestatud number on liiga suur.");
+            }
+            try
+            {
+                switch (operation)
+                {
+                    case "-":
+                        float resultAdd = firstNr - secondNr;
+                        Console.WriteLine("Kahe numbri lahutamisel on vastus: " + resultAdd);
+                        break;
+
+                    default:
+                        break;
+
+                }
+            }
+            catch (FormatException)//numbrilise v''rtuse kontroll
+            {
+                Console.WriteLine("Vale numbri formaat. Kas sa ei saa siis aru.");
+                //throw;
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("ERROR. Sisestatud number on liiga suur.");
+            }
+            try
+            {
+                switch (operation)
+                {
+                    case "*":
+                        float resultAdd = firstNr * secondNr;
+                        Console.WriteLine("Kahe numbri korrutamisel on vastus: " + resultAdd);
+                        break;
+
+                    default:
+                        break;
+
+                }
+            }
+            catch (FormatException)//numbrilise v''rtuse kontroll
+            {
+                Console.WriteLine("Vale numbri formaat. Kas sa ei saa siis aru.");
+                //throw;
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("ERROR. Sisestatud number on liiga suur.");
+            }
+            try
+            {
+                switch (operation)
+                {
+                    case "/":
+                        float resultAdd = firstNr / secondNr;
+                        Console.WriteLine("Kahe numbri jagamisel on vastus: " + resultAdd);
+                        break;
+
+                    default:
+                        break;
+
+                }
+            }
+            catch (FormatException)//numbrilise v''rtuse kontroll
+            {
+                Console.WriteLine("Vale numbri formaat. Kas sa ei saa siis aru.");
+                //throw;
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("ERROR. Sisestatud number on liiga suur.");
+            }
+
+
+        }
+    }
+}
